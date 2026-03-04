3b9586c26a1734ead600a4625303dfe847e66925
Gustavo Mendes de Moura <devgustavomendes@gmail.com>
2026-03-04 12:50:27 -0500

    Feature: Adicionado Estrutura de Decisão IF-ELSE no algoritmo.

---
+++ CLIENTES02.cob
@@ -0,0 +1,32 @@
+       
+       IDENTIFICATION      DIVISION.
+
+       PROGRAM-ID.  CLIENTES02.
+
+       ENVIRONMENT         DIVISION.
+       CONFIGURATION       SECTION.
+       SPECIAL-NAMES.
+           DECIMAL-POINT   IS COMMA.
+
+       DATA                DIVISION.
+       WORKING-STORAGE     SECTION.
+       01  WRK-NOME        PICTURE X(20)  VALUE "Gustavo".
+       01  WRK-SALDO       PICTURE S9(10) COMP VALUE ZEROS.
+       01  WRK-SALDO-ED    PICTURE Z.ZZZ.ZZZ.ZZ9,99.
+       01  WRK-TIPO-CONTA  PICTURE 9.
+
+       PROCEDURE           DIVISION.
+           DISPLAY "DIGITE SEU SALDO ".
+               ACCEPT WRK-SALDO.
+           DISPLAY "QUAL O TIPO DE CONTA (1-FISICA, 2-JURIDICA) ? ".
+               ACCEPT WRK-TIPO-CONTA.
+
+           IF WRK-SALDO IS GREATER THAN OR EQUAL TO 5000     
+               DISPLAY WRK-NOME
+               MOVE WRK-SALDO TO WRK-SALDO-ED
+               DISPLAY "SALDO" WRK-SALDO-ED
+           ELSE
+               DISPLAY "SALDO NAO COMPATIVEL"
+           END-IF.
+
+           STOP RUN.
