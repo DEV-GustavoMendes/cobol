3cf21e2159fbdbb7f5c8b5e33df0fcd97a0b0b70
Gustavo Mendes de Moura <devgustavomendes@gmail.com>
2026-03-10 15:42:34 -0400

    NEW: Estrutura de Evaluate inserido

--- CLIENTES02.cob
+++ CLIENTES02.cob
@@ -10,12 +10,14 @@
 
        DATA                DIVISION.
        WORKING-STORAGE     SECTION.
-       01  WRK-NOME        PICTURE X(20)  VALUE "Gustavo".
+       01  WRK-NOME        PICTURE X(20)  VALUE SPACES.
        01  WRK-SALDO       PICTURE S9(10) COMP VALUE ZEROS.
        01  WRK-SALDO-ED    PICTURE Z.ZZZ.ZZZ.ZZ9,99.
        01  WRK-TIPO-CONTA  PICTURE 9.
 
        PROCEDURE           DIVISION.
+           DISPLAY "DIGITE SEU NOME ".
+               ACCEPT WRK-NOME.
            DISPLAY "DIGITE SEU SALDO ".
                ACCEPT WRK-SALDO.
            DISPLAY "QUAL O TIPO DE CONTA (1-FISICA, 2-JURIDICA) ? ".
@@ -25,6 +27,16 @@
                DISPLAY WRK-NOME
                MOVE WRK-SALDO TO WRK-SALDO-ED
                DISPLAY "SALDO" WRK-SALDO-ED
+
+               EVALUATE WRK-TIPO-CONTA
+                   WHEN 1
+                       DISPLAY "CONTA FISICA"
+                   WHEN 2
+                       DISPLAY "CONTA JURIDICA"
+                   WHEN OTHER
+                      DISPLAY "TIPO DE CONTA NÃO EXISTE"
+               END-EVALUATE
+               
            ELSE
                DISPLAY "SALDO NAO COMPATIVEL"
            END-IF.
